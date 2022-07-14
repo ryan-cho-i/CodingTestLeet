9b0607a85c6d240113f7327ceff1a9ecd2a8c815
CodingStorageSoo <chlqudtn347@gmail.com>
2022-07-15 02:03:33 +0900

    7/15

---
+++ ex.cpp
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    
+    for (int i = 0 ; i < 10; i++){
+        cout << i ;
+    }
+    return 0;
+}
